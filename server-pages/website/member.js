3999b6652c34edf08ff5785d2c43f738908f0bb3
Janik Mayr <jan.222.ik@gmail.com>
2019-03-14 01:54:23 +0100

    ue4 begin

---
+++ server-pages/website/member.js
@@ -0,0 +1,11 @@
+function submitCheck() {
+    var fname = document.querySelector('#fname');
+    var lname = document.querySelector('#lname');
+    var address = document.querySelector('#address');
+    var username = document.querySelector('#username');
+    console.log(fname.value());
+    console.log(lname.value());
+    console.log(address.value());
+    console.log(username.value());
+}
+
