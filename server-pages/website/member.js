613649a2498e747b75dbf37d7d0106a12348ebf9
Janik Mayr <jan.222.ik@gmail.com>
2019-03-16 20:37:59 +0100

    ue3

--- server-pages/website/member.js
+++ server-pages/website/member.js
@@ -1,11 +1,27 @@
+console.log("On Load");
 function submitCheck() {
     var fname = document.querySelector('#fname');
     var lname = document.querySelector('#lname');
     var address = document.querySelector('#address');
     var username = document.querySelector('#username');
-    console.log(fname.value());
-    console.log(lname.value());
-    console.log(address.value());
-    console.log(username.value());
+    console.log(fname.value);
+    console.log(lname.value);
+    console.log(address.value);
+    console.log(username.value);
+}
+
+function checkPassword() {
+    var password = document.querySelector('#password');
+    console.log(password.value)
+}
+
+function checkPasswordRepeat() {
+    var password = document.querySelector('#password').value;
+    var passwordRepeat = document.querySelector('#passwordRepeat').value;
+    if (password === passwordRepeat) {
+        console.log("Passwords equal");
+    } else {
+        console.log("Passwords different length");
+    }
 }
 
