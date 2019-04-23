779d733e9dfb5892d3046750e2859949a260c567
Janik Mayr <jan.222.ik@gmail.com>
2019-04-23 22:28:20 +0200

    ue9 inter

--- webapplicationenWarProject/src/main/java/clazzes/CompetitionSupplierBean.java
+++ webapplicationenWarProject/src/main/java/clazzes/CompetitionSupplierBean.java
@@ -4,20 +4,22 @@ import org.jetbrains.annotations.Contract;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 
 @ManagedBean
 @SessionScoped
-public class CompetitionSupplierBean {
+public class CompetitionSupplierBean implements Serializable {
+    private static final long UID = 2L;
     @Contract(pure = true)
     public CompetitionSupplierBean() {
-        luftgewehr.add(new CompetitionEntry(1, LocalDateTime.of(2019,8,13, 20, 0), "Altötting"));
-        luftgewehr.add(new CompetitionEntry(3, LocalDateTime.of(2019,8,20, 20, 0), "Hurlach"));
-        luftgewehr.add(new CompetitionEntry(5, LocalDateTime.of(2019,8,27, 19, 0), "Burching"));
-        luftpistole.add(new CompetitionEntry(1, LocalDateTime.of(2019,9,1, 19, 0), "Burching"));
-        luftpistole.add(new CompetitionEntry(2, LocalDateTime.of(2019,9,6, 20, 0), "Hurlach"));
-        luftpistole.add(new CompetitionEntry(4, LocalDateTime.of(2019,10,15, 20, 0), "Altötting"));
+        luftgewehr.add(new CompetitionEntry(0, 1, LocalDateTime.of(2019,8,13, 20, 0), "Altötting"));
+        luftgewehr.add(new CompetitionEntry(1, 3, LocalDateTime.of(2019,8,20, 20, 0), "Hurlach"));
+        luftgewehr.add(new CompetitionEntry(2, 5, LocalDateTime.of(2019,8,27, 19, 0), "Burching"));
+        luftpistole.add(new CompetitionEntry(3, 1, LocalDateTime.of(2019,9,1, 19, 0), "Burching"));
+        luftpistole.add(new CompetitionEntry(4, 2, LocalDateTime.of(2019,9,6, 20, 0), "Hurlach"));
+        luftpistole.add(new CompetitionEntry(5, 4, LocalDateTime.of(2019,10,15, 20, 0), "Altötting"));
     }
 
     private LinkedList<CompetitionEntry> luftgewehr = new LinkedList<>();
@@ -34,12 +36,14 @@ public class CompetitionSupplierBean {
     }
 
     public class CompetitionEntry {
+        private int id;
         private int numberInSeason;
         private LocalDateTime date;
         private String location;
 
         @Contract(pure = true)
-        public CompetitionEntry(int numberInSeason, LocalDateTime date, String location) {
+        public CompetitionEntry(int id, int numberInSeason, LocalDateTime date, String location) {
+            this.id = id;
             this.numberInSeason = numberInSeason;
             this.date = date;
             this.location = location;
@@ -67,6 +71,10 @@ public class CompetitionSupplierBean {
 
         public void setNumberInSeason(int numberInSeason) {
             this.numberInSeason = numberInSeason;
+        }
+
+        public int getId() {
+            return id;
         }
     }
 
--- webapplicationenWarProject/src/main/java/clazzes/FacesFormBean.java
+++ webapplicationenWarProject/src/main/java/clazzes/FacesFormBean.java
@@ -4,14 +4,19 @@ import org.jetbrains.annotations.Contract;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.Map;
 
 @ManagedBean
 @SessionScoped
 public class FacesFormBean implements Serializable {
     private static final long UID = 1L;
     private String name;
-    private String discipline;
+    private String team;
+    private String participation;
+    private Boolean selfDrive;
+    private int trCompetitionId;
 
     @Contract(pure = true)
     public static long getUID() {
@@ -27,17 +32,53 @@ public class FacesFormBean implements Serializable {
         this.name = name;
     }
 
-    public String getDiscipline() {
-        return discipline;
+    public String getTeam() {
+        return team;
     }
 
-    public void setDiscipline(String discipline) {
-        System.out.println("set discipline: " + discipline);
-        this.discipline = discipline;
+    public void setTeam(String team) {
+        System.out.println("set team: " + team);
+        Map params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String eventId = (String) params.get("eventId");
+        System.out.println(eventId);
+        this.team = team;
     }
 
     public void submit() {
         System.out.println("submit");
         //return new Object();
+    }
+
+    public void setParticipation(String value) {
+        System.out.println(value);
+        System.out.println("ID: " + trCompetitionId);
+        participation = value;
+    }
+
+    public String getParticipation() {
+        return participation;
+    }
+
+    public Boolean getSelfDrive() {
+
+        return selfDrive;
+    }
+
+    public void setSelfDrive(Boolean selfDrive) {
+        System.out.println("Set self drive" + selfDrive);
+        System.out.println("ID: " + trCompetitionId);
+        //Map params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        //String selfDriveId = (String) params.get("selfDriveId");
+        //System.out.println(selfDriveId);
+        this.selfDrive = selfDrive;
+    }
+
+    public int getTrCompetitionId() {
+        return trCompetitionId;
+    }
+
+    public void setTrCompetitionId(int trCompetitionId) {
+        System.out.println("set id:" + trCompetitionId);
+        this.trCompetitionId = trCompetitionId;
     }
 }
